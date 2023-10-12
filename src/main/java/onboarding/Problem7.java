13a9141f2f489c7542b8554284f28932cae5e9ce
KimDabomi <cdabomi@nate.com>
2023-10-12 18:57:33 +0900

    friends < user 제외

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,38 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.regex.Pattern;
 
 public class Problem7 {
+    static Map<String, List<String>> friendsAndMutualFriends; // 친구와, 함께 아는 친구
+    static Map<String, Integer> friendsAndCommendScore; // 친구와, 추천 점수
+    static List<String> userFriends;
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        friendsAndMutualFriends = new HashMap<>();
+        friendsAndCommendScore = new HashMap<>();
+        userFriends = new ArrayList<>();
+
+        isUser(user, friends);
+
+
+        return userFriends;
     }
+
+    // User 제외
+    public static void isUser(String user, List<List<String>> friends) {
+        for (List<String> friend : friends) {
+            String friend1 = friend.get(0);
+            String friend2 = friend.get(1);
+
+            if (friend1.equals(user)) {
+                userFriends.add(friend2);
+            } else if (friend2.equals(user)) {
+                userFriends.add(friend1);
+            }
+        }
+    }
+
+
+
 }
