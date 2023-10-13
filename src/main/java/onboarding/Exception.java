3ff4e5082461519424d93fde98e78aed9cadc56c
KimDabomi <cdabomi@nate.com>
2023-10-13 20:38:43 +0900

    예외클래스 수정,

--- src/main/java/onboarding/Exception.java
+++ src/main/java/onboarding/Exception.java
@@ -8,8 +8,13 @@ public class Exception {
         return ListPage.get(0) + 1 != ListPage.get(1);
     }
 
-    public static boolean RightLength (List<Integer> ListPage, int max) {
+    public static boolean rightLength (List<Integer> ListPage, int max) {
         return ListPage.size() == max;
+    }
+
+    public static void rangeNumber (int number, int min_num, int max_num) {
+        if (number < min_num || number > max_num)
+            throw new IllegalArgumentException(number + "는 " + min_num + " 이상 " + max_num + " 이하인 자연수가 아닙니다.");
     }
 
 }
